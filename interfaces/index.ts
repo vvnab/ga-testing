972a299154400f41f71e7dc3c57a84254a6b1c6a
Виталий Набережный <neberezhniy@navalny-team.org>
2021-01-11 13:27:43 +0300

    --

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -5,6 +5,12 @@
 // import User from 'path/to/interfaces';
 
 export type User = {
-  id: number
-  name: string
+  id: number;
+  name: string;
+};
+
+declare global {
+  interface Window {
+    gtag: any;
+  }
 }
